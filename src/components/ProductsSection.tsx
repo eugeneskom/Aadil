155e2d7aaaf5b7deadad50684b79450d0bb81ec2
eugeneskom <eugeneskom@gmail.com>
2024-03-24 15:47:34 -0400

    added wishlist reducer, made a product a separate component

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -8,6 +8,7 @@ import { selectPage, incrementPage, fetchProductsAsync, selectProducts, selectPr
 import { NavLink } from "react-router-dom";
 import Image from "../images/bird.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import ProductCard from "./Product";
 
 const ProductSection = () => {
   const dispatch = useDispatch();
@@ -68,35 +69,7 @@ const ProductSection = () => {
         {products &&
           products.length > 0 &&
           products.map((product) => (
-            <div key={product.Id} className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
-              <LazyLoadImage src={product.ImageUrl} height={300} alt={product.Name} className="w-full max-h-[300px] object-contain" />
-              <div className="p-4 flex-grow relative">
-                <h3 className="text-lg font-semibold">{product.Name}</h3>
-                <p className="text-gray-600 mb-2 font-bold">
-                  <span className="text-gray-500 line-through">
-                    {product.OriginalPrice} {product.Currency}
-                  </span>
-                  {product.CurrentPrice && (
-                    <span className="text-red-500 ml-2">
-                      {product.CurrentPrice} {product.Currency}
-                    </span>
-                  )}
-                </p>
-                <p className="text-gray-500 mb-2">
-                  From: <span className="text-black font-bold">{product.CampaignName}</span>{" "}
-                </p>
-
-                <button className="absolute top-0 right-0 m-2 text-gray-500 hover:text-red-500 transition-colors duration-300">
-                  <FaHeart size={20} />
-                </button>
-              </div>
-
-              <div className="p-4 bg-gray-50">
-                <NavLink to={`/product-page/${product.Id}`} onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="w-full flex justify-center bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
-                  Read more
-                </NavLink>
-              </div>
-            </div>
+            <ProductCard product={product} />
           ))}
       </div>
 
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productSlice from '../state/products/productsSlice';
-
+import productSlice from "../state/products/productsSlice";
+import wishlistSlice from "./wishlist/wishlistSlice";
 
 export const store = configureStore({
   reducer: {
     products: productSlice,
+    wishlist: wishlistSlice,
     // Add other reducers here if needed
   },
 });
