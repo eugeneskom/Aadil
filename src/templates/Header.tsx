15caafe0a779cdc7668e0fd971c123f0e6047e67
eugeneskom <eugeneskom@gmail.com>
2024-03-29 13:49:29 -0400

    made nav responsive for tablets/mobiles

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -9,15 +9,30 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { IoIosArrowDown } from "react-icons/io";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { GoTag } from "react-icons/go";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Header = () => {
   const isValidToken = useSelector((state: RootState) => state.isValidToken.isValidToken);
   const isMobile = useSelector((state: RootState) => state.screenWidth.isMobile);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const wishlist: string[] = useSelector(selectWishlist);
+  const [openSubmenu, setOpenSubmenu] = useState("");
 
   const handleSearchToggle = () => {
     setIsSearchOpen((prev) => !prev);
   };
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+    setOpenSubmenu('');
+  };
+  const handleOpenSubmenu = (menu: string) => {
+    if(menu === openSubmenu) {
+      setOpenSubmenu('');
+      return;
+    }
+    setOpenSubmenu(menu);
+  }
+
   console.log("isMobile", isMobile);
 
   const googleAuth = () => {
@@ -28,10 +43,9 @@ const Header = () => {
     <header className="">
       <div className="container">
         <div className="header__inner py-4 px-6 flex items-center justify-between ">
-          {/* Logo */}
           {isMobile ? (
-            <button type="button" onClick={handleSearchToggle} className=" px-4 h-10 rounded-r-md  transition-colors duration-300">
-              <BiSearch size={24} />
+            <button onClick={handleToggleMenu}>
+              <RxHamburgerMenu size={30} className="cursor-pointer" />
             </button>
           ) : (
             ""
@@ -70,19 +84,31 @@ const Header = () => {
 
           {/* Navigation */}
           <nav className="flex items-center space-x-4">
+            {/* Search Mobile */}
+            {isMobile ? (
+              <button type="button" onClick={handleSearchToggle} className=" px-4 h-10 rounded-r-md  transition-colors duration-300">
+                <BiSearch size={24} />
+              </button>
+            ) : (
+              ""
+            )}
             {/* Notification Bell */}
-            <button className="header__action hover:text-gray-300 transition-colors duration-300">
-              <BiBell size={24} />
-            </button>
+            {!isMobile && (
+              <button className="header__action hover:text-gray-300 transition-colors duration-300">
+                <BiBell size={24} />
+              </button>
+            )}
 
             {/* Wishlist Heart */}
             <NavLink to="/wishlist" className="header__action hover:text-gray-300 transition-colors duration-300">
               <BiHeart size={24} />
               {wishlist && wishlist.length > 0 ? <span className="wishlist-counter"> {wishlist.length}</span> : ""}
             </NavLink>
-            <NavLink to="/user-account" className="header__action hover:text-gray-300 transition-colors duration-300">
-              <BiUser size={24} />
-            </NavLink>
+            {!isMobile && (
+              <NavLink to="/user-account" className="header__action hover:text-gray-300 transition-colors duration-300">
+                <BiUser size={24} />
+              </NavLink>
+            )}
 
             {/* <WishlistPopup /> */}
 
@@ -96,129 +122,142 @@ const Header = () => {
             )}
           </nav>
         </div>
-        <nav className="header__bottom px-6 nav-bottom">
-          <ul className="flex items-center justify-between">
-            <li className="nav-bottom__item">
-              <NavLink to="/" className="nav__link">
-                <span className="link-title sp-link-title">Home</span>
-              </NavLink>
-            </li>
-            <li className="nav-bottom__item">
-              <NavLink to="/products" className="nav__link">
-                <span className="link-title sp-link-title">Products</span>
-              </NavLink>
-            </li>
-            <li className="nav-bottom__item">
-              <NavLink to="/about" className="nav__link">
-                <span className="link-title sp-link-title">About</span>
-              </NavLink>
-            </li>
-            <li className="nav-bottom__item">
-              <NavLink to="/pages" className="nav__link">
-                <span className="link-title sp-link-title">Pages</span>
-                <IoIosArrowDown className="text-white arrow-down" />
-              </NavLink>
-              <ul className="menu-dropdown sub-menu collapse" id="sub-collapse-pages">
-                <li className="submenu-li">
-                  <a href="/pages/about-us-02" className="sublink-title">
-                    <span className="sp-link-title">About us</span>
-                    <span className="menu-arrow">
-                      <MdKeyboardArrowRight />
-                    </span>
-                  </a>
+        {isMenuOpen ? (
+          <nav className="header__bottom px-6 nav-bottom">
+            {isMobile ? (
+              <button type="button" onClick={handleToggleMenu}>
+                <IoMdClose className="cursor-pointer" size={30} />
+              </button>
+            ) : (
+              ""
+            )}
+            <ul className="header-botton-list flex items-center justify-between">
+              <li className="nav-bottom__item">
+                <NavLink to="/" className="nav__link">
+                  <span className="link-title sp-link-title">Home</span>
+                </NavLink>
+              </li>
+              <li className="nav-bottom__item">
+                <NavLink to="/products" className="nav__link">
+                  <span className="link-title sp-link-title">Products</span>
+                </NavLink>
+              </li>
+              <li className="nav-bottom__item">
+                <NavLink to="/about" className="nav__link">
+                  <span className="link-title sp-link-title">About</span>
+                </NavLink>
+              </li>
+              <li 
+              onClick={() => handleOpenSubmenu('pages')}
+              className={`nav-bottom__item ${openSubmenu === 'pages' ? 'active' : ""}`}>
+                <button className="nav__link">
+                  <span className="link-title sp-link-title">Pages</span>
+                  <IoIosArrowDown className="text-white arrow-down" />
+                </button>
+                <ul className="menu-dropdown sub-menu collapse" id="sub-collapse-pages">
+                  <li className="submenu-li">
+                    <NavLink to="/pages/about-us-02" className="sublink-title">
+                      <span className="sp-link-title">About us</span>
+                      {/* <span className="menu-arrow">
+                        <MdKeyboardArrowRight />
+                      </span> */}
+                    </NavLink>
 
-                  <ul className="supmenu-dropdown collapse" id="sup-collapse-about-us">
-                    <li className="supmenu-li">
-                      <a href="/pages/about-us" className="suplink-title">
-                        <span className="sp-link-title">About us 01</span>
-                      </a>
-                    </li>
+                    {/* <ul className="supmenu-dropdown collapse" id="sup-collapse-about-us">
+                      <li className="supmenu-li">
+                        <button type="button" className="suplink-title">
+                          <span className="sp-link-title">About us 01</span>
+                        </button>
+                      </li>
 
-                    <li className="supmenu-li">
-                      <a href="/pages/about-us-02" className="suplink-title">
-                        <span className="sp-link-title">About us 02</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
+                      <li className="supmenu-li">
+                        <button type="button" className="suplink-title">
+                          <span className="sp-link-title">About us 02</span>
+                        </button>
+                      </li>
+                    </ul> */}
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/contact" className="sublink-title">
-                    <span className="sp-link-title">Contact us</span>
-                    <span className="menu-arrow">
-                      <MdKeyboardArrowRight />
-                    </span>
-                  </a>
-                  <ul className="supmenu-dropdown collapse" id="sup-collapse-contact-us">
-                    <li className="supmenu-li">
-                      <a href="/pages/contact" className="suplink-title">
-                        <span className="sp-link-title">Contact us 01</span>
-                      </a>
-                    </li>
+                  <li className="submenu-li">
+                    <button type="button"  className="sublink-title">
+                      <span className="sp-link-title">Contact us</span>
+                      {/* <span className="menu-arrow">
+                        <MdKeyboardArrowRight />
+                      </span> */}
+                    </button>
+                    {/* <ul className="supmenu-dropdown collapse" id="sup-collapse-contact-us">
+                      <li className="supmenu-li">
+                        <NavLink to="/pages/contact" className="suplink-title">
+                          <span className="sp-link-title">Contact us 01</span>
+                        </NavLink>
+                      </li>
 
-                    <li className="supmenu-li">
-                      <a href="/pages/contact-us-02" className="suplink-title">
-                        <span className="sp-link-title">Contact us 02</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
+                      <li className="supmenu-li">
+                        <NavLink to="/pages/contact-us-02" className="suplink-title">
+                          <span className="sp-link-title">Contact us 02</span>
+                        </NavLink>
+                      </li>
+                    </ul> */}
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/faqs" className="sublink-title">
-                    <span className="sp-link-title">Faq's</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/faqs" className="sublink-title">
+                      <span className="sp-link-title">Faq's</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/return-policy" className="sublink-title">
-                    <span className="sp-link-title">Refund policy</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/return-policy" className="sublink-title">
+                      <span className="sp-link-title">Refund policy</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/delivery-return" className="sublink-title">
-                    <span className="sp-link-title">Shipping &amp; return</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/delivery-return" className="sublink-title">
+                      <span className="sp-link-title">Shipping &amp; return</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/store-location" className="sublink-title">
-                    <span className="sp-link-title">Store locations</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/store-location" className="sublink-title">
+                      <span className="sp-link-title">Store locations</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/privacy-policy" className="sublink-title">
-                    <span className="sp-link-title">Privacy policy</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/privacy-policy" className="sublink-title">
+                      <span className="sp-link-title">Privacy policy</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/pages/terms-condition" className="sublink-title">
-                    <span className="sp-link-title">Terms &amp; condition</span>
-                  </a>
-                </li>
+                  <li className="submenu-li">
+                    <NavLink to="/pages/terms-condition" className="sublink-title">
+                      <span className="sp-link-title">Terms &amp; condition</span>
+                    </NavLink>
+                  </li>
 
-                <li className="submenu-li">
-                  <a href="/404" className="sublink-title">
-                    <span className="sp-link-title">Page not found</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <div className="header__action-btns">
-            <button className="header__deals-today">
-              <FaRegBookmark />
-              <span>Deals today</span>
-            </button>
-            <button className="header__special-prices">
-            <GoTag />
-              <span>Special offers</span>
-            </button>
-          </div>
-        </nav>
+                  <li className="submenu-li">
+                    <NavLink to="/404" className="sublink-title">
+                      <span className="sp-link-title">Page not found</span>
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <div className="header__action-btns">
+              <button className="header__deals-today">
+                <FaRegBookmark />
+                <span>Deals today</span>
+              </button>
+              <button className="header__special-prices">
+                <GoTag />
+                <span>Special offers</span>
+              </button>
+            </div>
+          </nav>
+        ) : (
+          ""
+        )}
       </div>
     </header>
   );
